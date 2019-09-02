8c42c4a6ab3f39ba5418b031fe9fca596ae7fbb7
Pedro BM <p.bonilla.miralles@gmail.com>
2019-09-02 23:05:26 +0200

    input

--- converter/src/App.js
+++ converter/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h2>Converter</h2>
+        <input type='text' value='Type here'></input>
       </header>
     </div>
   );
